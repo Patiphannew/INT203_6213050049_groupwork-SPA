473dbc857d5149a29e4aa60c82af3283757a9ee3
Patiphan <patiphan.new@mail.kmutt.ac.th>
2021-04-17 23:43:41 +0700

    update 0.0.2

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,49 @@
 <template>
-  <div id="wrapper">
-    <div class="w-full h-28 shadow-lg bg-white">
-      <div
-        class="w-auto h-1/2 mx-auto flex flex-row item-center justify-center border-b-4 border-red-700"
-      >
-        <img class="my-auto mr-64" width="153" src="assets/Nismo.jpg" />
-        <ul class="my-auto ml-64 flex flex-row text-xs text-gray-500">
-          <li
-            class="px-2 py-1 rounded-full border-gray-400 hover:border-gray-300 hover:text-gray-400 border-2 flex item-center justify-center mr-2"
-          >
-            <a href>Japanese</a>
-          </li>
-          <li
-            class="px-2 py-1 rounded-full border-gray-400 hover:border-gray-300 hover:text-gray-400 border-2 flex item-center justify-center"
-          >
-            <a href>NISSAN</a>
-          </li>
-        </ul>
-      </div>
+  <div class="w-full h-28 shadow-lg bg-white">
+    <div
+      class="w-auto h-1/2 mx-auto flex flex-row item-center justify-center border-b-4 border-red-700"
+    >
+      <img class="my-auto mr-64" width="153" src="./assets/Nismo.jpg" />
+      <ul class="my-auto ml-64 flex flex-row text-xs text-gray-500">
+        <li
+          class="px-2 py-1 rounded-full border-gray-400 hover:border-gray-300 hover:text-gray-400 border-2 flex item-center justify-center mr-2"
+        >
+          <a href>Japanese</a>
+        </li>
+        <li
+          class="px-2 py-1 rounded-full border-gray-400 hover:border-gray-300 hover:text-gray-400 border-2 flex item-center justify-center"
+        >
+          <a href>NISSAN</a>
+        </li>
+      </ul>
+    </div>
+    <div
+      class="w-auto h-1/2 mx-auto flex flex-row item-center justify-center border-b-4 border-red-700"
+    >
+      <ul class="flex flex-row item-center justify-center h-full text-gray-700 text-sm">
+        <li class="pr-3 py-3">
+          <i class="icon material-icons">settings</i>
+          <span>
+            <b>PRODUCTS</b>
+          </span>
+        </li>
+        <li class="py-3">
+          <span class="border-r-2 border-l-2 border-gray-400 my-auto px-3">
+            <i class="icon material-icons">av_timer</i>
+            <span>
+              <b>MOTORSPORTS</b>
+            </span>
+          </span>
+        </li>
+        <li class="pl-3 py-3">
+          <i class="icon material-icons">info</i>
+          <span>
+            <b>About NISMO</b>
+          </span>
+        </li>
+      </ul>
     </div>
   </div>
+
   <router-view />
 </template>
-
-<!-- /* <style>
-.icon {
-  font-size: 17px;
-}
-body {
-  background-image: url("img/bg_main.png");
-}
-.content {
-  width: 860px;
-  font-weight: bold;
-}
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
-</style> */ -->
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="w-full h-16 mt-5 text-4xl text-red-800 font-medium">Original website.</div>
+    <iframe src="https://www.nismo.co.jp/en/index.html" class="h-screen w-full"></iframe>
   </div>
 </template>
